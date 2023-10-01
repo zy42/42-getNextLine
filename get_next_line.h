72e21f396a9e063e84c330b703db88e2dffce0b9
zy42 <zengying1942@gmail.com>
2023-10-02 02:43:13 +0800

    update header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,28 @@
 /*   By: yzeng <yzeng@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 02:17:56 by yzeng             #+#    #+#             */
-/*   Updated: 2023/10/01 02:20:21 by yzeng            ###   ########.fr       */
+/*   Updated: 2023/10/02 02:42:55 by zengying         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-char    *find_next_line(char *buffer);
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct  Files
+{
+        int     id;
+        int     read_status;
+        char    *buffer;
+        char    *line;
+        size_t  byte;
+} files;
+
 char	*get_next_line(int fd);
+char    *find_next_line(struct Files *file);
 size_t  ft_strlen(const char *s);
 char    *char_memcpy(char *src, size_t n);
 
